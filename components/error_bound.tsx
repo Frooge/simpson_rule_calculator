d28e0e490f551f52fbe7b153da2ec2e0e6964acc
Marc Valeros <marcnathanielvaleros@gmail.com>
2024-05-04 00:51:24 +0800

    Update error_bound.tsx

--- components/error_bound.tsx
+++ components/error_bound.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { MathJax } from 'better-react-mathjax';
-import { derivative } from 'mathjs';
+import { derivative, parse, ceil } from 'mathjs';
+import AnswerComponent from '@/components/answer_component';
 
 interface props {
     answerValue: number,
@@ -13,13 +14,52 @@ interface props {
 export default function ErrorBound ({
     minValue, maxValue, functionValue, epsilon} : props
 ) {
+    const a = parseInt(minValue);
+    const b = parseInt(maxValue);
+    const eps = parseFloat(epsilon);
 
+    const findDerivatives  = () => {
+        if(!functionValue) return;
 
+        let derivs: string[] = [];
+        let currentFunction: string = functionValue;
+        for (let i = 0; i <= 3; i++) {
+            derivs.push(derivative(currentFunction,'x').toString())
+            currentFunction = derivs[i]
+        }
+        return derivs;
+    }
+
+    const findMaxValue = (f:string = functionValue) => {
+        let M: number = 0;
+        let fourth_deriv = derivative(derivative(derivative(derivative(functionValue,'x'),"x"),"x"),"x").toString();
+        const xValues = [a,b]
+        let value = xValues.map(x => parse(fourth_deriv).evaluate({x:x}))
+        return M = Math.max(...(value).filter(x => x !== Infinity));
+    }
+
+    const findN = (M: number) => {
+        let N: number;
+        N = parse(`((${M}*((${b}-${a})^5))/(180*${eps}))^(1/4)`).evaluate();
+        return (N !== Infinity) ? N : NaN;
+    }
 
     return (
-        <div className='text-white text-[18px]'>
-        <div>Here <MathJax inline>{`$a = ${minValue}, b=${maxValue}, f(x) = ${functionValue}$`}</MathJax></div>
-        <div>Error Formula for the Simpson&apos;s Rule: <MathJax inline>{`$ bar{e} leq frac{M(b-a)^5}{180 n^4}$`}</MathJax></div>
+        <div className='text-white text-[18px] flex flex-col space-y-6'>
+        <MathJax inline>{`Here $a = ${a}, b=${b}, f(x) = ${functionValue}$`}</MathJax>
+        <MathJax inline>{`Error Formula:  $bar{e} leq frac{M(b-a)^5}{180 n^4}$ `}</MathJax>
+        <MathJax inline>{`Rearranging the variables gives: $n geq  4sqrt{frac{M(b-a)^5}{180 bar{e}}}$`}</MathJax>
+        <MathJax inline>{`Where  $M$ is the maximum value of $|f^{(4)}(x)|$`}</MathJax>
+        <div>Then, {
+                findDerivatives()?.map((item,index,array) => (
+                    <span key={index}>
+                        <MathJax inline>{`$f^${index+1}(x) = ${item} ${index < array.length - 1 ? "," : " ."} $`}</MathJax>
+                    </span>
+                ))}
+        </div>
+        <MathJax inline>{`Therefore we set $M$ = $${findMaxValue()}$, since $|f^{(4)}(x)| leq$ $${findMaxValue()}$ for $${a}$ $leq x leq$ ${b}`}</MathJax>
+        <MathJax inline>{`Solve for $n$,  $n geq  4sqrt{frac{${findMaxValue()}(${b}-${a})^5}{180(${eps})}} =  ${findN(findMaxValue())} = ${ceil(findN(findMaxValue()))}  $ iterations `}</MathJax>
+
         </div>
     )
 }
